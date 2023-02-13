44eae1dacece8dee35d608f48c8c708c63fe136e
Thiago Francelino <tfrancelino65@gmail.com>
2023-02-13 16:15:07 -0300

    [ADD] inherit_routes: modulo criado para poder acrescentar o dia da semana na rota, ex: segunda, terça e etc, o mesmo cria um model e inherita routes entao sao duas class no mesmo arquivo .py fiz assim por serem poucas coisas a serem adicionadas. cotacao: foi criada a funcao calcula_vencimento, a mesma modifica a data de vencimento de acordo com o dia da rota do cliente, por exemplo se o motorista vai para determinada rota segunda e sexta e a cotacao esta sendo feita na terça então a data de vencimento deve ser a proxima sexta-feira

--- wizard/cotacao.py
+++ wizard/cotacao.py
@@ -1,5 +1,6 @@
 from odoo import models, fields, api, _
 from datetime import date
+import datetime
 
 
 class CotacoesVendas(models.TransientModel):
@@ -27,6 +28,31 @@ class CotacoesVendas(models.TransientModel):
     partner_quotes = fields.Many2many(
         comodel_name='sale.order',
     )
+
+    @api.onchange('partner_route_id')
+    def calcula_vencimento(self):
+        today = datetime.date.today()
+        weekday = today.weekday()
+
+        days_of_week = ["Segunda-feira",
+                        "Terça-feira",
+                        "Quarta-feira",
+                        "Quinta-feira",
+                        "Sexta-feira",
+                        "Sábado",
+                        "Domingo"]
+        dia_rota = []
+        for rec in self.partner_route_id.dia_rota:
+            dia_rota.append(rec.dia)
+        if self.partner_route_id:
+            while days_of_week[today.weekday()] != dia_rota[-1]:
+                today += datetime.timedelta(days=1)
+            self.expire_date = today
+
+
+
+
+
 
     @api.onchange('partner_id')
     def part_quotes(self):
